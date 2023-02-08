93b5a2d0eb0c13a3847e94b9acc899e32a5cf8fd
Lapo0 <lapo.gini.01@gmail.com>
2023-02-08 15:11:09 +0100

    FEAT human-code palindromo

--- js/app.js
+++ js/app.js
@@ -5,10 +5,19 @@ console.log('funzia?')
 ******************/
 
 // chiedere al'utente di inserire una parola
+    // usare promt
 
 // creare una funzione per capire se la parola è palindroma
+    // trasformare tutti i caretteri minuscoli
+    // trasformare la parola in un array
+    // invertire gli indici dell'array
+    // confrontare tutti gli indici
+        // usando una comparazione tra gli indici del primo array e del secondo con le lettere invertite
+
+
 
 // comunicare all'utente se la parola inserita è palindroma o no
+    // console.log
 
 /*****************
  PARI E DISPARI
