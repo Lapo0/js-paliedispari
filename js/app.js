e6d11c7405f8856721c5fc9eb12f0d67d2e187d6
Lapo0 <lapo.gini.01@gmail.com>
2023-02-08 15:28:45 +0100

    FEAT promt & lowerCase & Array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('funzia?')
+// console.log('funzia?')
 
 /*****************
  PALINDROMO
@@ -6,14 +6,30 @@ console.log('funzia?')
 
 // chiedere al'utente di inserire una parola
     // usare promt
+const parolaUtente = prompt('Inserisci una parola palindroma');
+console.log(parolaUtente)
+
+// invocare la funzione
+isPalindrome(parolaUtente)
 
 // creare una funzione per capire se la parola è palindroma
-    // trasformare tutti i caretteri minuscoli
+    
     // trasformare la parola in un array
     // invertire gli indici dell'array
     // confrontare tutti gli indici
         // usando una comparazione tra gli indici del primo array e del secondo con le lettere invertite
+ 
+function isPalindrome (parola) {
 
+    // trasformare tutti i caretteri minuscoli
+    const lowerCaseParola = parola.toLowerCase()
+    console.log(lowerCaseParola)
+
+    const parolaArray = [...lowerCaseParola]
+    console.log(parolaArray)
+
+
+ }
 
 
 // comunicare all'utente se la parola inserita è palindroma o no
