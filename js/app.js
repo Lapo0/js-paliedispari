969b45a1f28736790703eea61d983d427aec3d5b
Lapo0 <lapo.gini.01@gmail.com>
2023-02-08 15:06:56 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log('funzia?')
+
+/*****************
+ PALINDROMO
+******************/
+
+// chiedere al'utente di inserire una parola
+
+// creare una funzione per capire se la parola è palindroma
+
+// comunicare all'utente se la parola inserita è palindroma o no
+
+/*****************
+ PARI E DISPARI
+******************/
+
+// far scegliere all'utente pari o disapri
+// far scegliere all'utente un numero tra 1 e 5
+
+// generare un numero random da 1 a 5 usando una funzione
+
+// sommiamo i due numeri
+
+// stambilire se la somma dei due numeri è pari o disapri usando una funzione
+
+// comunicare il risultato all'utente
